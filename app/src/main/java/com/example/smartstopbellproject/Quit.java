52ccc2a8f9c86d5e1132fe30c6b4417b92b31e38
DESKTOP-583LQSG\82102 <unbbang0487@naver.com>
2021-11-09 22:51:30 +0900

    파이어베이스 추가

---
+++ app/src/main/java/com/example/smartstopbellproject/Quit.java
@@ -0,0 +1,32 @@
+package com.example.smartstopbellproject;
+
+public class Quit {
+
+    private String bId;
+    private int count;
+    private String sName;
+
+    public String getbId() {
+        return bId;
+    }
+
+    public void setbId(String bId) {
+        this.bId = bId;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public String getsName() {
+        return sName;
+    }
+
+    public void setsName(String sName) {
+        this.sName = sName;
+    }
+}
