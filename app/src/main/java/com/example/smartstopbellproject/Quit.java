ae3e155bd85ee4c7b0aa9e1443d8f97fe75f887f
DESKTOP-583LQSG\82102 <unbbang0487@naver.com>
2021-11-10 16:24:53 +0900

    DB 정보 추가

--- app/src/main/java/com/example/smartstopbellproject/Quit.java
+++ app/src/main/java/com/example/smartstopbellproject/Quit.java
@@ -1,10 +1,24 @@
 package com.example.smartstopbellproject;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Quit {
 
     private String bId;
     private int count;
     private String sName;
+    private Map<String, Boolean> stars = new HashMap<>();
+
+    public Quit() {
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
+
+    public Quit(String bId, int count, String sName) {
+        this.bId = bId;
+        this.count = count;
+        this.sName = sName;
+    }
 
     public String getbId() {
         return bId;
@@ -28,5 +42,17 @@ public class Quit {
 
     public void setsName(String sName) {
         this.sName = sName;
+    }
+
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("uid", uid);
+        result.put("author", author);
+        result.put("title", title);
+        result.put("body", body);
+        result.put("starCount", starCount);
+        result.put("stars", stars);
+
+        return result;
     }
 }
