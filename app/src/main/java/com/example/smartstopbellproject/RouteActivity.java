38e7008d77a7ba89a920afc5e1695a1c430fc14f
MangoJinyoung <9132225@gmail.com>
2021-11-16 03:37:38 +0900

    노선 데이터 가져오기, 예약&예약취소 기능

--- app/src/main/java/com/example/smartstopbellproject/RouteActivity.java
+++ app/src/main/java/com/example/smartstopbellproject/RouteActivity.java
@@ -1,5 +1,6 @@
 package com.example.smartstopbellproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
@@ -18,12 +19,23 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.annotations.Nullable;
+
 public class RouteActivity extends AppCompatActivity {
 
     ListView listview;
     ListViewAdapter adapter;
     TextView busNum;
     String selectStop;
+    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    DatabaseReference databaseReference = firebaseDatabase.getReference();
+    ChildEventListener ChildEventListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +43,7 @@ public class RouteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_route);
 
         Button btnCancel = (Button) findViewById(R.id.btnCancel);
+        busNum = findViewById(R.id.busNum);
 
         // Adapter 생성
         adapter = new ListViewAdapter();
@@ -39,11 +52,29 @@ public class RouteActivity extends AppCompatActivity {
         listview = findViewById(R.id.listview);
         listview.setAdapter(adapter);
 
+        DatabaseReference route = firebaseDatabase.getReference("busroute");
+        route.child("2222").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    String str = snapshot.child("stopname").getValue(String.class);
+                    adapter.addItem(R.drawable.route1, str);
+                    busNum.setText(str);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
         // 임시로 데이터 추가해놓음
-        adapter.addItem(R.drawable.route_start, "첫번째 정류장");
-        adapter.addItem(R.drawable.route1, "두번째 정류장");
-        adapter.addItem(R.drawable.route1, "세번째 정류장");
-        adapter.addItem(R.drawable.route_end, "네번째 정류장");
+//        adapter.addItem(R.drawable.route_start, "첫번째 정류장");
+//        adapter.addItem(R.drawable.route1, "두번째 정류장");
+//        adapter.addItem(R.drawable.route1, "세번째 정류장");
+//        adapter.addItem(R.drawable.route_end, "네번째 정류장");
 
         /* 정민이꺼
         busNum = findViewById(R.id.busNum);
@@ -71,15 +102,15 @@ public class RouteActivity extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 //예약 버튼 클릭시 동작
-                listview.setSelector(new PaintDrawable(0xE2FFD6C8));
+                listview.setSelector(R.color.pink);
                 Toast.makeText(getApplicationContext(), "예약완료",Toast.LENGTH_LONG).show();
+
+                //db에 예약 정보 추가
+                databaseReference.child("reserve").setValue(selectStop);
 
                 //예약취소 버튼 활성화
                 btnCancel.setVisibility(View.VISIBLE);
 
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                intent.putExtra("route",selectStop);
-                startActivity(intent);
             }
         });
 
@@ -97,25 +128,50 @@ public class RouteActivity extends AppCompatActivity {
             }
         });
 
+        //예약취소 버튼 클릭 이벤트
         btnCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //예약취소 버튼 클릭시 동작
+                Toast.makeText(getApplicationContext(), "예약취소",Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
+
+                //db에서 삭제
+                databaseReference.child("reserve").removeValue();
             }
         });
-
 
         //뒤로가기버튼 동작
         ImageButton back = findViewById(R.id.back);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                intent.putExtra("route",selectStop);
-                startActivity(intent);
+                finish();
             }
         });
+
+        //예약취소 하지 않은 경우
+//        DatabaseReference reserve = firebaseDatabase.getReference("reserve");
+//        if(reserve!= null){
+//            listview.setSelector(R.color.pink);
+//            btnCancel.setVisibility(View.VISIBLE);
+//        }
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                for(DataSnapshot data : snapshot.getChildren()){
+                    if (data.child("reserve").exists()){
+                        listview.setSelector(R.color.pink);
+                        btnCancel.setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+
+            }
+        });
+
     }
 }
